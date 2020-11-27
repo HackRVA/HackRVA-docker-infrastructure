1b37915904f24924565c18ff0816123fb6a06132
Dustin Firebaugh <dafirebaugh@gmail.com>
2020-11-27 12:23:27 -0500

    including the wiki iamges dir in the backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -6,6 +6,10 @@ filename=${basefilename}_$(date +%Y-%m-%d_%H-%M-%S).tar.gz
 
 # tar up the db files
 tar -zcvf ${filename} /var/lib/mysql/
+# tar up the images dir
+tar -zcvf ${filename}"_images" /var/www/html/images
 
 # backup database to NAS!
 cp ${filename} /backup/NAS/wiki/
+# backup images to NAS!
+cp ${filename}"_iamges" /backup/NAS/wiki/
